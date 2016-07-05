ce866bb063ae9fa3f36bb61a0c46a281c569603a
Juan Salas <juan@amigocloud.com>
2016-07-05 08:19:10 -0500

    Added TCP client

--- TcpClient.cpp
+++ TcpClient.cpp
@@ -2,7 +2,16 @@
 
 
 TcpClient::TcpClient() {
+    this->server_port = 8080;
+    this->server_ip = "127.0.0.1";
 
+    this->sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    memset(&this->addr, 0, sizeof(struct sockaddr_in));
+    this->addr.sin_family = AF_INET;
+    this->addr.sin_port = htons(this->server_port);
+    inet_pton(AF_INET, this->server_ip.c_str(), &this->addr.sin_addr);
+
+    connect(this->sock, (const struct sockaddr *)&this->addr, sizeof(struct sockaddr_in));
 }
 
 TcpClient::~TcpClient() {
@@ -10,9 +19,35 @@ TcpClient::~TcpClient() {
 }
 
 void TcpClient::sendRequest(std::string request) {
+    std::cout<<"Working"<<std::endl;
+    std::string first = std::to_string(request.size());
 
+    if (send(this->sock, first.c_str(), first.size(), 0) < 0) {
+        perror("Send failed : ");
+        return;
+    }
+    if (send(this->sock, first.c_str(), first.size(), 0) < 0) {
+        perror("Send failed : ");
+        return;
+    }
+
+    std::cout<<"Working done"<<std::endl;
 }
 
 std::string TcpClient::getResponse() {
-    return "";
+    char buffer_size[10] = {0};
+    if(recv(this->sock, buffer_size, 10, 0) < 0) {
+        perror("Mismatch in number of bytes received");
+    }
+
+    std::string bs(buffer_size);
+
+    int buff_size = std::stoi(bs);
+    std::cout<<buff_size<<std::endl;
+    std::string response(buff_size+1, 0);
+    if(recv(this->sock, &response[0], buff_size, 0) < 0) {
+        perror("Mismatch in number of bytes received");
+    }
+    std::cout<<response;
+    return response;
 }
--- TcpClient.h
+++ TcpClient.h
@@ -2,7 +2,9 @@
 #define HTTP_UDP_TCPCLIENT_H
 
 #include <string>
-
+#include <cstring>
+#include <iostream>
+#include <arpa/inet.h>
 
 class TcpClient {
 public:
@@ -15,8 +17,10 @@ public:
     std::string getResponse();
 
 private:
-    int socket_fd;
-
+    int sock;
+    int server_port;
+    struct sockaddr_in addr;
+    std::string server_ip;
 };
 
 
